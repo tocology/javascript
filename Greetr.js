6507f330d7c50aa94b99308c5454c9b8603b788f
JunHo Hwang <hwangjun7777@gmail.com>
2016-03-26 04:31:27 +0000

    feat: Add more functions in Greetr

--- Greetr.js
+++ Greetr.js
@@ -4,7 +4,76 @@
         return new Greetr.init(firstName, lastName, language);
     }
     
-    Greetr.prototype = {}
+    var supportedLangs = ['en', 'es']
+    
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola',
+    }
+    
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos',
+    }
+    
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Iniciar la sesión'
+    }
+    
+    Greetr.prototype = {
+        fullName: function() {
+            return this.firstName + ' ' + this.lastName;
+        },
+        
+        validate: function() {
+            if(supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language"
+            }
+        },
+        
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstName + '!'
+        },
+        
+        formalGreeting: function() {
+            return formalGreetings[this.language] + ', ' + this.fullName()
+        },
+        
+        greet: function(formal) {
+            var msg
+            
+            // if undefined or null it will be coerced to 'false'
+            if(formal) {
+                msg = this.formalGreeting()
+            } else {
+                msg = this.greeting()
+            }
+            
+            if(console) {
+                console.log(msg)
+            }
+            
+            // 'this' refers to the calling object at execution time
+            // make the method chainable
+            return this
+        },
+        
+        log: function() {
+            if (console) {
+                console.log(logMessages[this.language] + ': ' + this.fullName())
+            }
+            
+            return this
+        },
+        
+        setLang: function(lang) {
+            this.language = lang
+            this.validate()
+            
+            return this
+        },
+    }
     
     Greetr.init = function(firstName, lastName, language) {
         
--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-var g = G$(firstname, lastname, language)
+var g = G$('John','Deo')
+g.greet().setLang('es').greet(true)
